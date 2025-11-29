d11dff702c8e166e0e864dfb077619d93a386d8c
Lluis <lluisunyol@gmail.com>
2025-11-29 12:29:13 +0100

    Fixed "no module named ai_tracking issue

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from datetime import datetime
 from pathlib import Path
 
 import torch
+
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(__file__)
+SRC_DIR = os.path.join(CURRENT_DIR, "src")
+
+if SRC_DIR not in sys.path:
+    sys.path.insert(0, SRC_DIR)
+
 from ai_tracking import run_yolo_on_video, run_yolo_tracking_mode
 
 
